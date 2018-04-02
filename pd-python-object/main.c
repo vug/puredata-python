94c3f868ce20b78607b9144edc1ef0bed35eea3f
Ugur Guney <ugurguney@gmail.com>
2018-04-01 23:27:55 -0400

    init runpython PD object with fixed Makefile.

--- pd-python-object/main.c
+++ pd-python-object/main.c
@@ -1,4 +1,6 @@
+#include <stdlib.h>
 #include "m_pd.h"
+#include "Python.h"
 
 
 static t_class *runpython_class;
@@ -14,8 +16,43 @@ void runpython_bang(t_runpython *x) {
 }
 
 
+char * getString(PyObject * obj) {
+  PyObject *str_of_obj = PyObject_Str(obj);
+  PyObject *temp_bytes = PyUnicode_AsEncodedString(str_of_obj, "UTF-8", "strict");
+  char *str = PyBytes_AS_STRING(temp_bytes);
+  // my_result = strdup(s1);  // from: <string.h>
+  return str;
+}
+
+
 void *runpython_constructor(void) {
   t_runpython *obj = (t_runpython *)pd_new(runpython_class);  /* Parent's constructor */
+
+
+
+  int runState;
+  char *str_to_pd = (char*)malloc(100 * sizeof(char));
+  char *result_str;
+  PyObject* locals = PyDict_New();
+  PyObject* globals = PyDict_New();
+  PyObject* resultObject;
+
+  post("generating new helloworld object...");
+
+  Py_Initialize();
+
+  PyMapping_SetItemString(globals, "time", PyImport_ImportModule("time"));
+  resultObject = PyRun_String(
+    "time.ctime(time.time())\n",
+     Py_eval_input,
+     locals,
+     globals
+  );
+  result_str = getString(resultObject);
+  sprintf(str_to_pd, "PyRun_String result: %s", result_str);
+  post(str_to_pd);
+
+
 
   return (void *)obj;
 }
